1634ede1914fe94df77636b7ea92db1d211f2167
RedOrion <redorion0@icloud.com>
2018-02-12 10:50:46 -0500

    Add: SQL setup script

---
+++ setup/sql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo mysql -u root -p -h localhost << EOF
+CREATE DATABASE sensors;
+USE sensors;
+CREATE TABLE tempData (temp_id INT AUTO_INCREMENT KEY, takeDate DATE, takeTime TIME, zone VARCHAR(64), temperature DOUBLE);
+CREATE USER 'sensors'@'localhost' IDENTIFIED by 'testPassword';                                                
+GRANT ALL PRIVILEGES ON sensors.* TO 'sensors'@'localhost';
+FLUSH PRIVILEGES;
+
+EOF
