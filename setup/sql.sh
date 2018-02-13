2d2cbae8eec81c42002d604fcdbd0f957213cc8c
RedOrion <redorion0@icloud.com>
2018-02-12 23:05:25 -0500

    Update setup script

--- setup/sql.sh
+++ setup/sql.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo mysql -u root -p -h localhost << EOF
-CREATE DATABASE sensors;
+CREATE DATABASE IF NOT EXISTS sensors;
 USE sensors;
-CREATE TABLE tempData (temp_id INT AUTO_INCREMENT KEY, takeDate DATE, takeTime TIME, zone VARCHAR(64), temperature DOUBLE);
-CREATE USER 'sensors'@'localhost' IDENTIFIED by 'testPassword';                                                
+CREATE TABLE IF NOT EXISTS tempData (temp_id INT AUTO_INCREMENT KEY, takeDate DATE, takeTime TIME, zone VARCHAR(64), temperature DOUBLE);
+CREATE USER IF NOT EXISTS 'sensors'@'localhost' IDENTIFIED by 'testPassword';                                                
 GRANT INSERT ON sensors.* TO 'sensors'@'localhost';
 FLUSH PRIVILEGES;
 
