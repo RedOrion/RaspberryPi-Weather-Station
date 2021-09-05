d7c49248baed5c212a28df45841011581b829d8d
RedOrion <github@northcountryelectronics.com>
2021-09-05 10:37:23 -0500

    Update sql.sh

--- setup/sql.sh
+++ setup/sql.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-sudo mysql -u root -p -h localhost << EOF
+while getopts s: flag
+do
+    case "${flag}" in
+        s) mysqlserver=${OPTARG};;
+    esac
+done
+echo "MysqlServer: $mysqlserver";
+
+sudo mysql -u root -p -h $mysqlserver << EOF
 CREATE DATABASE IF NOT EXISTS sensors;
 USE sensors;
 CREATE TABLE IF NOT EXISTS tempData (temp_id INT AUTO_INCREMENT KEY, takeDate DATE, takeTime TIME, zone VARCHAR(64), temperature DOUBLE);
-CREATE USER IF NOT EXISTS 'sensors'@'localhost' IDENTIFIED by 'testPassword';                                                
+CREATE USER IF NOT EXISTS 'sensors'@'localhost' IDENTIFIED by 'sensorsTest';                                                
 GRANT INSERT ON sensors.* TO 'sensors'@'localhost';
-CREATE USER IF NOT EXISTS 'website'@'localhost' IDENTIFIED by 'passwordTest';
+CREATE USER IF NOT EXISTS 'website'@'localhost' IDENTIFIED by 'websiteTest';
 GRANT SELECT ON sensors.* TO 'website'@'localhost';
 FLUSH PRIVILEGES;
 
