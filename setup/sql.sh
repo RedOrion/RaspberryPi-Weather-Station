3be7ac7cb41739b70cf6ffb9994bc796b29b9051
RedOrion <github@northcountryelectronics.com>
2021-09-05 16:33:39 -0500

    Update sql.sh

--- setup/sql.sh
+++ setup/sql.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-while getopts s: flag
+while getopts s:w: flag
 do
     case "${flag}" in
         s) mysqlserver=${OPTARG};;
+        w) weatherstationpi=${OPTARG};;
     esac
 done
 echo "MysqlServer: $mysqlserver";
+echo "Weather Station Pi: $weatherstationpi";
 
 sudo mysql -u root -p -h $mysqlserver << EOF
 CREATE DATABASE IF NOT EXISTS sensors;
 USE sensors;
 CREATE TABLE IF NOT EXISTS tempData (temp_id INT AUTO_INCREMENT KEY, takeDate DATE, takeTime TIME, zone VARCHAR(64), temperature DOUBLE);
-CREATE USER IF NOT EXISTS 'sensors'@'localhost' IDENTIFIED by 'sensorsTest';                                                
-GRANT INSERT ON sensors.* TO 'sensors'@'localhost';
-CREATE USER IF NOT EXISTS 'website'@'localhost' IDENTIFIED by 'websiteTest';
-GRANT SELECT ON sensors.* TO 'website'@'localhost';
+CREATE USER IF NOT EXISTS 'sensors'@'$weatherstationpi' IDENTIFIED by 'sensorsTest';                                                
+GRANT INSERT ON sensors.* TO 'sensors'@'$weatherstationpi';
+CREATE USER IF NOT EXISTS 'website'@'$weatherstationpi' IDENTIFIED by 'websiteTest';
+GRANT SELECT ON sensors.* TO 'website'@'$weatherstationpi';
 FLUSH PRIVILEGES;
 
 EOF
