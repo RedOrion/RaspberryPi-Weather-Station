23017071a75532fcfea665a347f6281ec1bc6c1b
Bernard <bkluskens@spikehorncreek.com>
2022-02-15 18:18:59 -0500

    new sql

---
+++ server/temperature-read.py
@@ -0,0 +1,20 @@
+import pymysql
+from credentials import User, Password, Database, DatabaseServer
+
+# To connect MySQL database
+conn = pymysql.connect(host=DatabaseServer,
+        user=User,
+        passwd=Password,
+        db=Database)
+    
+cur = conn.cursor()
+    
+# Select query
+cur.execute("select * from sensors")
+output = cur.fetchall()
+    
+for i in output:
+    print(i)
+    
+# To close the connection
+conn.close()
